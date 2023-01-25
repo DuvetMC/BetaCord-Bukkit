3556fed9b01be425708038db3a0c589ffc0c50cb
KJP12 <kjp12.k.joy.programer12@gmail.com>
2023-01-24 21:25:26 -0600

    Exclude Quilt Libraries from being fed through Knot.

--- gameProvider/src/main/kotlin/BukkitLibrary.kt
+++ gameProvider/src/main/kotlin/BukkitLibrary.kt
@@ -3,7 +3,22 @@ package de.olivermakesco.betacord.quilt.provider
 import net.fabricmc.api.EnvType
 
 enum class BukkitLibrary : Library {
-    BUKKIT("org/bukkit/craftbukkit/Main.class");
+    BUKKIT("org/bukkit/craftbukkit/Main.class"),
+    QUILT_LIBRARIES(
+        "org/quiltmc/loader/impl/launch/knot/Knot.class",
+        "net/fabricmc/loader/launch/server/FabricServerLauncher.class",
+        "org/quiltmc/json5/JsonReader.class",
+        "org/quiltmc/config/api/Config.class",
+        "org/objectweb/asm/util/ASMifier.class",
+        "org/objectweb/asm/tree/ClassNode.class",
+        "org/objectweb/asm/commons/JSRInlinerAdapter.class",
+        "org/objectweb/asm/tree/analysis/Frame.class",
+        "org/objectweb/asm/Opcodes.class",
+        "net/fabricmc/accesswidener/AccessWidener.class",
+        "org/spongepowered/asm/mixin/Debug.class",
+        "net/fabricmc/tinyremapper/AsmRemapper.class",
+        "net/fabricmc/mapping/tree/TinyTree.class"
+    );
 
     private val envType: EnvType?
     private val paths: List<String>
